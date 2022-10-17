f5fbc6fd06d5f14884ea97386e4e518d234aea79
Sally-N <snzungula@gmail.com>
2022-10-17 03:34:51 -0700

    last

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,6 @@
 #include<unistd.h>
 /**
- * Description - main prints the standard error sandwiched around the standard outpu/t
+ * Main - prints the standard error sandwiched around the standard output
  *
  * Return: returns 1
  */
