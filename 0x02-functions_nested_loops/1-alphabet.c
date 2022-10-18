8fe7bec30e4ea08aef2a97dfd00b20e84a4d7b3c
Sally Nzungula <snzungula@gmail.com>
2022-10-18 21:18:06 +0000

    letters

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,21 +2,20 @@
 
 /**
  * print_alphabet - prints alphabet lowercase
- * 
+ *
  * Return: Always 0: Success
  */
 
 void print_alphabet(void)
 {
 	char letter;
-	
+
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		_putchar(letter);
 	}
-			
+	
 	_putchar('\n');
-
+	
 }	
 
-	
