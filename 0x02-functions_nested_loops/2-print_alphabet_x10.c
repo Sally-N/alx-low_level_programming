50f1e38c515f765f7e6e4ee6fa09a0899b3d5b9b
Sally Nzungula <snzungula@gmail.com>
2022-10-18 21:46:41 +0000

    print 10 of alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10x
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet_x10(void)
+{
+	int round = 0;
+	char letter = 'a';
+
+	while(round < 10)
+	{
+		letter = 'a';
+	while (letter <= z)
+	 
+	while (letter <= 'z'){
+
+	_putchar(letter);
+	letter++
+
+	}
+	_putchar('\n');
+
+	round++;
+	}
+}
+		
