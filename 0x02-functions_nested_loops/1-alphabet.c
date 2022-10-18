208085bd3c00555a04c5a3dbf60058f9bead68ae
Sally Nzungula <snzungula@gmail.com>
2022-10-18 20:59:57 +0000

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints  alphabet lowercas
+ *
+ * Return: Always 0: Success
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+	
+	for (letter = 'a'; letter <= 'z'; letter++)
+	{
+		_putchar(letter)
+	}
+			
+	_putchar('\n');
+
+}	
+
+	
