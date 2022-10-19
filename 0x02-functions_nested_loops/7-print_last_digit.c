29ade39547edd6c620c0409fa5f547e7aa908dea
Sally Nzungula <snzungula@gmail.com>
2022-10-19 12:31:39 +0000

    message

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
 /**
  * print_last_digit - prints the last digit of a number
  * @r: The number in question
- * Return: value of the last digit 
+ * Return: value of the last digit
  */
 
 int print_last_digit(int r)
