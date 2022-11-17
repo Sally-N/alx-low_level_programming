9dd336f23ee5052a50a5d0037c41ace34201d422
Sally-N <snzungula@gmail.com>
2022-11-17 00:49:15 +0000

    function that initialize a variable of type

--- 0x0E-structures_typedef/1-init_gog.c
+++ 0x0E-structures_typedef/1-init_gog.c
@@ -12,14 +12,14 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-		if (d == NULL)
-					return;
+	if (d == NULL)
+	return;
 
-			else
-					{
-								d->name = name;
-										d->age = age;
-												d->owner = owner;
-													}
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 
 }
