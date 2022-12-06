575ea86edf18385c16f6390ef3a7c118d9387f9b
Sally-N <snzungula@gmail.com>
2022-12-06 18:46:43 +0000

    function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,9 +2,9 @@
 #define MAIN_H
 
 /*
- *  * Desc: Header file containing prototypes for all functions
- *   *       written in the 0x13-bit_manipulation directory.
- *    */
+ * Desc: Header file containing prototypes for all functions
+ * written in the 0x13-bit_manipulation directory.
+ */
 
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
