e4bc4b4538a6603726cf3208e359b0b6dc389691
Sally-N <snzungula@gmail.com>
2023-02-08 09:49:22 +0000

    hash-tables

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,18 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+
+/**
+ * key_index - function to calculate the index of a key
+ *
+ * @key: key of the node
+ * @size: size of the array
+ * Return: index at which the key/value pair should be stored in the array of the hash table
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int index;
+
+	index = (hash_djb2(key) % size);
+	return (index);
+}
