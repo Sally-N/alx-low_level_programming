9a1ce468f1af2358be0e685666b44f00b46450f7
Sally-N <snzungula@gmail.com>
2023-05-10 14:32:19 +0000

    files

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,7 +1,7 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
 
-/*Desc: Header file*/
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 
