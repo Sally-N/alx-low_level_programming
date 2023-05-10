2a323e8feb81ec1f4d5aac59c08781e13b0b2da6
Sally-N <snzungula@gmail.com>
2023-05-10 14:24:18 +0000

    files

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -9,7 +9,8 @@
  * @array: pointer to the first element of the array to search in.
  * @size: number of elements in array.
  * @value: value to search for. 
- * Return: first index where value is located, -1 otherwise.
+ * Return: first index where value is located
+ * 	   -1 if value is not present in array or array is NULL.
  */
 
 int linear_search(int *array, size_t size, int value)
